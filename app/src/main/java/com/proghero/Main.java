773194b53fa36d0725a9f3300f4c04daa60172a8
root <root@localhost>
2024-12-01 20:22:58 +0000

    auto publish 1733084578560

--- app/src/main/java/com/proghero/Main.java
+++ app/src/main/java/com/proghero/Main.java
@@ -1,7 +1,13 @@
 //filename:Example.java
+import java.util.Scanner;
+
 class Example {
     public static void main(String args[]) {
-        boolean isGuest = true;
-        System.out.println(isGuest);
+        String name;
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        name = userInput.nextLine();
+        System.out.println("Hi, " + name);
+        userInput.close();
     }
 }
